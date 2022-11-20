0464898b6695f8877ad89240c296b8c11b6ab671
Srishti <srishtiparajuli2056@gmail.com>
2022-11-20 22:54:17 +0545

    register-client complete

--- src/pages/ClientRegistration/BudgetAdd.js
+++ src/pages/ClientRegistration/BudgetAdd.js
@@ -1,7 +1,5 @@
 import Navbar from "components/Navbar/Navbar";
-
-import budget from 'assets/images/budget-rate-icon.jpg';
-import rateImg from 'assets/images/budget-time-icon.png';
+import Input from "components/Input/Input";
 import Button from 'components/Button/Button';
 import { useNavigate } from "react-router-dom";
 
@@ -14,48 +12,28 @@ const Budget = () => {
     return(
         <>
       <Navbar/>
-      <div className="container overflow-hidden section-choice">
-      <h5 className="headline-color" >4/4 Budget</h5>
-        <h1 className="page-title mb-2">Tell us about your budget</h1>
-        <h5 className="page-description">
+      <div className="container w-50 add-skills-page ">
+      <h5 className="headline-color mt-5" >4/4 Budget</h5>
+        <h1 className="page-title mb-2 mt-4">Tell us about your budget</h1>
+        <h4 className=" mt-3">
         This will help us match you to talent within your range.
-        </h5>
-        <div className="row text-center row-cols-1 row-cols-md-1 row-cols-lg-2 gx-5 gy-5">
-          <div
-            role="button"
-            tabIndex={0}
-            className="rate-div choice pt-2"
-          >
-            <img
-              className="rate-img choice-img"
-              src={rateImg}
-              alt="time"
-            />
-            <p className="choice-img__description client-img__description">
-              Hourly Rate
-            </p>
+        </h4>
+        <form className="mt-5" >
+          <Input
+            label="Enter an hourly rate:"
+            className="mb-2"
+            type="text"
+            placeholder="$0.00"
+          />
+          <p className="mb-5">Professionals tend to charge $30 - $50/hour (USD) for projects like yours.</p>
+          <div >             
           </div>
-          <div
-            role="button"
-            tabIndex={0}
-            className="choice-freelancer choice pt-2"
-          >
-            <img
-              className="time-img choice-img"
-              src={budget}
-              alt="rate"
-            />
-            <p className="choice-img__description client-img__description">
-              Project Rate
-            </p>
-          </div>
-        </div>
-        <div className="text-center choice-btn mt-5">
+        </form>
+        <div className="text-end">
           <Button
           onClick={navigateToDescription}
-            className="btn-round "
-            type="submit"
-            label=" Add Description"
+            className="btn btn-registration btn-round"
+            label="Next"
           />
         </div>
       </div>
