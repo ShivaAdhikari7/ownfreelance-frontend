19b4aabfea68b12d93f9331384372179486313b6
Dikshak Poudel <dikshak@truemark.com.np>
2022-12-01 21:32:23 +0545

    resolve issues

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import Signup from "pages/Signup/Signup";
+import Signup from 'pages/Signup/Signup';
 
-import GettingStarted from "pages/Signup/GettingStarted";
-import EducationForm from "pages/FreelancerRegistration/EducationForm";
-import FreelancerPreferences from "pages/FreelancerRegistration/FreelancerPreferences";
-import WorkExperience from "pages/FreelancerRegistration/WorkExperienceForm";
-import VerifyOtp from "pages/OTP/VerifyOtp";
-import SuccessfulOtp from "pages/OTP/SuccessfulOtp";
-import TitleAdd from "pages/FreelancerRegistration/TitleAdd";
-import SkillsAdd from "pages/FreelancerRegistration/SkillsAdd";
-import BioAdd from "pages/FreelancerRegistration/BioAdd";
-import HourlyRateAdd from "pages/FreelancerRegistration/HourlyRateAdd";
-import Headline from "pages/ClientRegistration/HeadlineAdd";
-import Skills from "pages/ClientRegistration/SkillsAdd";
-import Scope from "pages/ClientRegistration/ScopeAdd";
-import Budget from "pages/ClientRegistration/BudgetAdd";
-import Description from "pages/ClientRegistration/DescriptionAdd";
-import Login from "pages/UserLogin/UserLogin";
-import Dashboard from "pages/Dashboard/Dashboard";
-import VerificationSuccessful from "pages/Signup/VerificationSuccessful";
-import ProfileView from "pages/FreelancerRegistration/ProfileView";
-import HomePage from "pages/HomePage/HomePage";
+import GettingStarted from 'pages/Signup/GettingStarted';
+import EducationForm from 'pages/FreelancerRegistration/EducationForm';
+import FreelancerPreferences from 'pages/FreelancerRegistration/FreelancerPreferences';
+import WorkExperience from 'pages/FreelancerRegistration/WorkExperienceForm';
+import VerifyOtp from 'pages/OTP/VerifyOtp';
+import SuccessfulOtp from 'pages/OTP/SuccessfulOtp';
+import TitleAdd from 'pages/FreelancerRegistration/TitleAdd';
+import SkillsAdd from 'pages/FreelancerRegistration/SkillsAdd';
+import BioAdd from 'pages/FreelancerRegistration/BioAdd';
+import HourlyRateAdd from 'pages/FreelancerRegistration/HourlyRateAdd';
+import Headline from 'pages/ClientRegistration/HeadlineAdd';
+import Skills from 'pages/ClientRegistration/SkillsAdd';
+import Scope from 'pages/ClientRegistration/ScopeAdd';
+import Budget from 'pages/ClientRegistration/BudgetAdd';
+import Description from 'pages/ClientRegistration/DescriptionAdd';
+import Login from 'pages/UserLogin/UserLogin';
+import Dashboard from 'pages/Dashboard/Dashboard';
+import VerificationSuccessful from 'pages/Signup/VerificationSuccessful';
+import ProfileView from 'pages/FreelancerRegistration/ProfileView';
+import HomePage from 'pages/HomePage/HomePage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/otp/success" element={<SuccessfulOtp />} />
       <Route path="/otp/verify" element={<VerifyOtp />} />
-      <Route path="" element={<Signup />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/user/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/getstarted" element={<GettingStarted />} />
