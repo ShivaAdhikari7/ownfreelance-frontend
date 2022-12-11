91dc8f2f3b8d03e8cc45cacad0b4ace13e15a13e
Dikshak Poudel <dikshak@truemark.com.np>
2022-12-11 16:02:55 +0545

    add pagination functionality

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState, useEffect, useCallback } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { MdOutlineRssFeed } from 'react-icons/md';
 
@@ -8,12 +7,54 @@ import SearchResultFreelancer from 'components/Search/SearchResultFreelancer';
 import SearchResultClient from 'components/Search/SearchResultClient';
 
 import LoadingSpinner from 'components/Spinner/LoadingSpinner';
+import Pagination from 'components/Pagination/Pagination';
+
+import getSearchResults from 'api/getSearchResults';
 
 const Search = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [totalResults, setTotalResults] = useState(0);
   const [searchQuery, setSearchQuery] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [finalQuery, setFinalQuery] = useState('');
+
+  useEffect(() => {
+    let timer;
+    let delay = 750;
+
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      setFinalQuery(searchQuery);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [searchQuery]);
+
+  const getPaginatedResults = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const [totalResults, searchResults] = await getSearchResults(
+        finalQuery,
+        currentPage
+      );
+
+      setIsLoading(false);
+      setSearchResults(searchResults);
+      setTotalResults(totalResults);
+    } catch (err) {
+      setIsLoading(false);
+    }
+  }, [currentPage, finalQuery]);
+
+  useEffect(() => {
+    const getResults = async () => {
+      await getPaginatedResults();
+    };
+
+    getResults();
+  }, [getPaginatedResults]);
 
   const searchQueryChangeHandler = e => {
     setSearchQuery(e.target.value);
@@ -22,47 +63,7 @@ const Search = () => {
   const searchSubmitHandler = async e => {
     e.preventDefault();
 
-    let data;
-
-    try {
-      setIsLoading(true);
-
-      if (localStorage.getItem('userType') === 'Client') {
-        ({ data } = await axios.get(
-          `http://localhost:90/freelancer?keyword=${searchQuery}`,
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('__token__')}`,
-            },
-          }
-        ));
-
-        setIsLoading(false);
-      } else if (localStorage.getItem('userType') === 'Freelancer') {
-        ({ data } = await axios.get(
-          `http://localhost:90/client?keyword=${searchQuery}`,
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('__token__')}`,
-            },
-          }
-        ));
-
-        setIsLoading(false);
-      }
-
-      console.log(data);
-
-      let freelancers =
-        localStorage.getItem('userType') === 'Client'
-          ? data.data.freelancers
-          : data.data.clients;
-
-      setTotalResults(data.data.result);
-      setSearchResults(freelancers);
-    } catch (err) {
-      setIsLoading(false);
-    }
+    await getPaginatedResults(finalQuery, currentPage);
   };
 
   return (
@@ -94,44 +95,53 @@ const Search = () => {
               </div>
 
               <div className="search-stats d-flex align-items-center mb-2">
-                <MdOutlineRssFeed />{' '}
+                <MdOutlineRssFeed />
                 <span>
                   <strong>{totalResults}</strong>{' '}
                   {localStorage.getItem('userType') === 'Freelancer'
                     ? 'Jobs'
-                    : 'Freelancers'}{' '}
+                    : `Freelancer${totalResults > 1 ? 's ' : ' '}`}
                   found
                 </span>
               </div>
-              <div className="search-results d-flex flex-column">
-                {!isLoading ? (
-                  searchResults.map(result =>
-                    localStorage.getItem('userType') === 'Client' ? (
-                      <SearchResultClient
-                        key={result._id}
-                        profilePictureUrl={result.profilePictureUrl}
-                        firstName={result.userId.firstName}
-                        lastName={result.userId.lastName}
-                        hourlyRate={result.hourlyRate}
-                        bio={result.bio}
-                        skills={result.skills}
-                        jobTitle={result.jobTitle}
-                      />
-                    ) : localStorage.getItem('userType') === 'Freelancer' ? (
-                      <SearchResultFreelancer
-                        headline={result.headline}
-                        skills={result.skills}
-                        hourlyRate={result.hourlyRate}
-                        description={result.description}
-                        projectDuration={result.scope.projectDuration}
-                        projectSize={result.scope.projectSize}
-                      />
-                    ) : (
-                      ''
+              <div className="search-result-section">
+                <div className="search-results d-flex flex-column">
+                  {!isLoading ? (
+                    searchResults.map(result =>
+                      localStorage.getItem('userType') === 'Client' ? (
+                        <SearchResultClient
+                          key={result._id}
+                          profilePictureUrl={result.profilePictureUrl}
+                          firstName={result.userId.firstName}
+                          lastName={result.userId.lastName}
+                          hourlyRate={result.hourlyRate}
+                          bio={result.bio}
+                          skills={result.skills}
+                          jobTitle={result.jobTitle}
+                        />
+                      ) : localStorage.getItem('userType') === 'Freelancer' ? (
+                        <SearchResultFreelancer
+                          headline={result.headline}
+                          skills={result.skills}
+                          hourlyRate={result.hourlyRate}
+                          description={result.description}
+                          projectDuration={result.scope.projectDuration}
+                          projectSize={result.scope.projectSize}
+                        />
+                      ) : (
+                        ''
+                      )
                     )
-                  )
-                ) : (
-                  <LoadingSpinner />
+                  ) : (
+                    <LoadingSpinner />
+                  )}
+                </div>
+                {searchResults.length > 1 && (
+                  <Pagination
+                    className="w-50 ml-auto justify-content-end mt-5"
+                    currentPage={currentPage}
+                    currentPageChangeHandler={setCurrentPage}
+                  />
                 )}
               </div>
             </div>
